7095d2c7b3ed3e957930cd62456df768da9e4259
jansenv <jansenv44@gmail.com>
2020-03-13 14:44:26 -0500

    trying to add plants

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -4,6 +4,7 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
+using Trestlebridge.Models.Plants;
 
 namespace Trestlebridge.Actions
 {
@@ -57,6 +58,32 @@ namespace Trestlebridge.Actions
                     break;
                 case 7:
                     ChooseGrazingField.CollectInput(farm, new Sheep());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void CollectPlantInput(Farm farm)
+        {
+            var plantList = new List<IResource>();
+            plantList.Add(new Sesame());
+
+            foreach (var plant in plantList)
+            {
+                Console.WriteLine($"{plantList.IndexOf(plant)}. {plant}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+
+            switch (Int32.Parse(choice))
+            {
+                case 1:
+                    ChooseGrazingField.CollectInput(farm, new Chicken());
                     break;
                 default:
                     break;
