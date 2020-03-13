1fe34b6de4b8e91f89e753b6289b63dbd6446b62
kevinpenny3 <kevin.penny3@yahoo.com>
2020-03-13 12:20:52 -0500

    added grazing field selection

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -12,7 +12,9 @@ namespace Trestlebridge.Actions
         public static void CollectInput(Farm farm)
         {
             var animalList = new List<IResource>();
+            animalList.Add(new Chicken());
             animalList.Add(new Cow());
+            animalList.Add(new Duck());
             animalList.Add(new Goat());
             animalList.Add(new Ostrich());
             animalList.Add(new Pig());
@@ -32,7 +34,29 @@ namespace Trestlebridge.Actions
             switch (Int32.Parse(choice))
             {
                 case 1:
+
+                    // create chooseChickenHouse
+                    ChooseGrazingField.CollectInput(farm, new Chicken());
+                    break;
+                case 2:
                     ChooseGrazingField.CollectInput(farm, new Cow());
+                    break;
+                case 3:
+
+                    // create chooseDuckHouse
+                    ChooseGrazingField.CollectInput(farm, new Duck());
+                    break;
+                case 4:
+                    ChooseGrazingField.CollectInput(farm, new Goat());
+                    break;
+                case 5:
+                    ChooseGrazingField.CollectInput(farm, new Ostrich());
+                    break;
+                case 6:
+                    ChooseGrazingField.CollectInput(farm, new Pig());
+                    break;
+                case 7:
+                    ChooseGrazingField.CollectInput(farm, new Sheep());
                     break;
                 default:
                     break;
