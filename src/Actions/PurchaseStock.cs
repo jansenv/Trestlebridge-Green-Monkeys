d6fbd4ac442cfe228fcd2b985a1e43fb243d565f
jansenv <jansenv44@gmail.com>
2020-03-13 15:25:02 -0500

    idk

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -82,7 +82,7 @@ namespace Trestlebridge.Actions
             switch (Int32.Parse(choice))
             {
                 case 1:
-                    ChooseGrazingField.CollectInput(farm, new Chicken());
+                    ChooseGrazingField.CollectInput(farm, new Sesame());
                     break;
                 default:
                     break;
