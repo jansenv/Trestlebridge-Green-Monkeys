0e745e3e16ec2f41248ce4afb483acf75c5d8b65
kevinpenny3 <kevin.penny3@yahoo.com>
2020-03-13 10:47:30 -0500

    addind animal instances

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -1,20 +1,33 @@
 using System;
+using System.Collections.Generic;
 using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseStock {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+namespace Trestlebridge.Actions
+{
+    public class PurchaseStock
+    {
+        public static void CollectInput(Farm farm)
+        {
+            var animalList = new List<IResource>();
+            animalList.Add(new Chicken());
+            animalList.Add(new Cow());
+            animalList.Add(new Duck());
+            animalList.Add(new Goat());
+            animalList.Add(new Ostrich());
+            animalList.Add(new Pig());
+            animalList.Add(new Sheep());
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine("1. Cow");
+            Console.WriteLine("2. Ostrich");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+            Console.WriteLine();
+            Console.WriteLine("What are you buying today?");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
 
             switch (Int32.Parse(choice))
             {
