6271767a84e6994c587d13b2fbb5b500ad188378
jansenv <jansenv44@gmail.com>
2020-03-14 15:03:57 -0500

    all of the plants can be put inside of their corresponding fields, except for the plowed field which is not able to accept the sunflower yet

--- src/Models/Facilities/PlowedField.cs
+++ src/Models/Facilities/PlowedField.cs
@@ -3,37 +3,42 @@ using System.Collections.Generic;
 using System.Text;
 using Trestlebridge.Interfaces;
 
-namespace Trestlebridge.Models.Facilities {
-  public class PlowedField : IFacility<ISeedProducing> {
-    private int _rowCapacity = 13;
-    private int _plantPerRowCapacity = 5;
-    private Guid _id = Guid.NewGuid ();
+namespace Trestlebridge.Models.Facilities
+{
+    public class PlowedField : IFacility<ISeedProducing>
+    {
+        private int _rowCapacity = 13;
+        private int _plantPerRowCapacity = 5;
+        private Guid _id = Guid.NewGuid();
 
-    private List<ISeedProducing> _seeds = new List<ISeedProducing> ();
+        private List<ISeedProducing> _seeds = new List<ISeedProducing>();
 
-    public double Capacity {
-      get {
-        return _rowCapacity * _plantPerRowCapacity;
-      }
+        public double Capacity
+        {
+            get
+            {
+                return _rowCapacity * _plantPerRowCapacity;
+            }
+        }
+        public void AddResource(ISeedProducing seed)
+        {
+            _seeds.Add(seed);
+        }
+
+        public void AddResource(List<ISeedProducing> seed)
+        {
+            // TODO: implement this...
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
+
+            output.Append($"Plowed field {shortId} has {this._seeds.Count} plants\n");
+            this._seeds.ForEach(a => output.Append($"   {a}\n"));
+
+            return output.ToString();
+        }
     }
-    public void AddResource (ISeedProducing seed) {
-      // TODO: implement this...
-      throw new NotImplementedException ();
-    }
-
-    public void AddResource (List<ISeedProducing> seed) {
-      // TODO: implement this...
-      throw new NotImplementedException ();
-    }
-
-    public override string ToString () {
-      StringBuilder output = new StringBuilder ();
-      string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
-
-      output.Append ($"Plowed field {shortId} has {this._seeds.Count} plants\n");
-      this._seeds.ForEach (a => output.Append ($"   {a}\n"));
-
-      return output.ToString ();
-    }
-  }
 }
