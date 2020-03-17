5b15fab7d03d87d990730c2c29c682325dedca9e
onteriowright <onteriowright@yahoo.com>
2020-03-17 11:12:42 -0500

    added try and catch to choose duck house, chicken house, and grazing field

--- src/Actions/ChooseChickenHouse.cs
+++ src/Actions/ChooseChickenHouse.cs
@@ -4,27 +4,53 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 
-namespace Trestlebridge.Actions {
-    public class ChooseChickenHouse {
-        public static void CollectInput (Farm farm, IEggProducing animal) {
-            Utils.Clear ();
+namespace Trestlebridge.Actions
+{
+    public class ChooseChickenHouse
+    {
+        public static void CollectInput(Farm farm, IEggProducing animal)
+        {
+            Utils.Clear();
+            Console.WriteLine($"Place the animal where?");
 
-            Console.WriteLine ($"Place the animal where?");
+            while (true)
+            {
+                try
+                {
+                    static void chickenHouseSelect(Farm farm, IEggProducing animal)
+                    {
+                        var filteredChickenHouses = farm.ChickenHomes.Where(home => home.Capacity > home.AnimalCount).ToList();
 
-            static void chickenHouseSelect (Farm farm, IEggProducing animal) {
-                var filteredChickenHouses = farm.ChickenHomes.Where (home => home.Capacity > home.AnimalCount).ToList ();
+                        if (filteredChickenHouses.Count() == 0)
+                        {
+                            Console.WriteLine("No houses are avaliable. Please add another house...");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
 
-                for (int i = 0; i < filteredChickenHouses.Count (); i++) {
-                    Console.Write ($"{i+1}. Chicken House {filteredChickenHouses[i].ShortId} has {filteredChickenHouses[i].AnimalCount} chickens");
+                            for (int i = 0; i < filteredChickenHouses.Count(); i++)
+                            {
+                                Console.Write($"{i+1}. Chicken House {filteredChickenHouses[i].ShortId} has {filteredChickenHouses[i].AnimalCount} chickens");
 
-                    Console.WriteLine ();
+                                Console.WriteLine();
+                            }
+
+                            int choice = Int32.Parse(Console.ReadLine());
+                            filteredChickenHouses[choice - 1].AddResource(animal);
+                        }
+
+                    }
+
+                    chickenHouseSelect(farm, animal);
+                    break;
                 }
-
-                int choice = Int32.Parse (Console.ReadLine ());
-                filteredChickenHouses[choice - 1].AddResource (animal);
+                catch (Exception)
+                {
+                    Console.WriteLine($"Please make a valid selection! Press the return key...");
+                    Console.ReadLine();
+                }
             }
-
-            chickenHouseSelect (farm, animal);
 
         }
     }
--- src/Actions/ChooseDuckHouse.cs
+++ src/Actions/ChooseDuckHouse.cs
@@ -4,27 +4,52 @@ using Trestlebridge.Interfaces;
 using Trestlebridge.Models;
 using Trestlebridge.Models.Animals;
 
-namespace Trestlebridge.Actions {
-    public class ChooseDuckHouse {
-        public static void CollectInput (Farm farm, IEggProducing animal) {
-            Utils.Clear ();
+namespace Trestlebridge.Actions
+{
+    public class ChooseDuckHouse
+    {
+        public static void CollectInput(Farm farm, IEggProducing animal)
+        {
+            Utils.Clear();
 
-            Console.WriteLine ($"Place the animal where?");
+            Console.WriteLine($"Place the animal where?");
 
-            static void duckHouseSelect (Farm farm, IEggProducing animal) {
-                var filteredDuckHouses = farm.DuckHomes.Where (home => home.Capacity > home.AnimalCount).ToList ();
+            while (true)
+            {
+                try
+                {
+                    static void duckHouseSelect(Farm farm, IEggProducing animal)
+                    {
+                        var filteredDuckHouses = farm.DuckHomes.Where(home => home.Capacity > home.AnimalCount).ToList();
 
-                for (int i = 0; i < filteredDuckHouses.Count (); i++) {
-                    Console.Write ($"{i+1}. Duck House {filteredDuckHouses[i].ShortId} has {filteredDuckHouses[i].AnimalCount} ducks");
+                        if (filteredDuckHouses.Count() == 0)
+                        {
+                            Console.WriteLine("No houses are avaliable. Please add another house...");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
 
-                    Console.WriteLine ();
+                            for (int i = 0; i < filteredDuckHouses.Count(); i++)
+                            {
+                                Console.Write($"{i+1}. Duck House {filteredDuckHouses[i].ShortId} has {filteredDuckHouses[i].AnimalCount} ducks");
+
+                                Console.WriteLine();
+                            }
+
+                            int choice = Int32.Parse(Console.ReadLine());
+                            filteredDuckHouses[choice - 1].AddResource(animal);
+                        }
+                    }
+                    duckHouseSelect(farm, animal);
+                    break;
                 }
-
-                int choice = Int32.Parse (Console.ReadLine ());
-                filteredDuckHouses[choice - 1].AddResource (animal);
+                catch (Exception)
+                {
+                    Console.WriteLine($"Please make a valid selection! Press the return key...");
+                    Console.ReadLine();
+                }
             }
-
-            duckHouseSelect (farm, animal);
 
         }
     }
